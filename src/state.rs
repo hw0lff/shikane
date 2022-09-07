0e7277139e03ff7f02ed5d914fe2e7f3913679e8
Hendrik Wolff <hendrik.wolff@agdsn.me>
2022-09-07 23:48:05 +0200

    state: Add handling the Done event for the TestingProfile state

--- src/state.rs
+++ src/state.rs
@@ -167,7 +167,11 @@ impl ShikaneState {
                 self.select_next_profile_then_configure_and_test()
             }
             (State::StartingUp, StateInput::OutputConfigurationSucceeded) => todo!(),
-            (State::TestingProfile, StateInput::OutputManagerDone) => todo!(),
+            (State::TestingProfile, StateInput::OutputManagerDone) => {
+                // OutputManager sent new information about current configuration
+                self.create_list_of_unchecked_profiles();
+                self.select_next_profile_then_configure_and_test()
+            }
             (State::TestingProfile, StateInput::OutputConfigurationSucceeded) => {
                 // Profile passed testing
                 self.configure_selected_profile();
