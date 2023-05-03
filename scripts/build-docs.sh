6ec787daed92c0e82578d306eab2d0762e13c515
Hendrik Wolff <hendrik.wolff@agdsn.me>
2023-05-03 15:58:05 +0200

    docs: Optionally specify version via argument for build-docs.sh man
    Remove the hardcoded version numbers from shikane.{1,5}.man.yml

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -6,16 +6,20 @@ docs="$shikane_dir/docs"
 out="$shikane_dir/build"
 
 
+## Build man pages from markdown files, gzip and write them to $out directory
+## $1: shikane version included in the man pages
 buildman() {
-    mkdir -p "$out"
+    local version="$1"
     local manpages=("shikane.1" "shikane.5")
     local opts=(
         --standalone
         --from markdown --to man
         --template "$docs/shikane.man.template"
         -V "date=$(date +%F)"
+        -V "footer=shikane $version"
     )
 
+    mkdir -p "$out"
     for page in "${manpages[@]}"; do
         local page_opts=(
             --metadata-file "$docs/$page.man.yml"
@@ -33,21 +37,29 @@ cleanman() {
 
 usage() {
     cat << EOF
-Usage:
-    man   - build man pages
-    clean - remove man pages
+Usage: build-docs.sh <COMMAND>
+
+Commands:
+  man [VERSION]  - build man pages [explicitly use <VERSION> string]
+  clean          - remove man pages
 EOF
 }
 
 
-if [[ $# -eq 1 ]]; then
+if [[ $# -eq 1 || $# -eq 2 ]]; then
     case $1 in
         clean)
             cleanman
             exit 0;;
         man)
-            buildman
-            exit 0;;
+            if [[ $# -eq 2 ]]; then
+                buildman "$2"
+            else
+                version="$(grep -m 1 version Cargo.toml | cut -d\" -f2)"
+                buildman "$version"
+            fi
+            exit 0
+            ;;
         *)
             usage
             exit 1;;
