feaba14bf53630567e1778e6d2a14350706e27df
Hendrik Wolff <hendrik.wolff@agdsn.me>
2023-05-29 00:38:37 +0200

    error: Add report function

--- src/error.rs
+++ src/error.rs
@@ -27,3 +27,17 @@ impl<T> From<calloop::InsertError<T>> for ShikaneError {
         ShikaneError::EventLoop(err.into())
     }
 }
+
+pub(crate) fn report(error: &dyn snafu::Error) -> String {
+    let sources = snafu::ChainCompat::new(error);
+    let sources: Vec<&dyn snafu::Error> = sources.collect();
+    let sources = sources.iter().rev();
+    let mut s = String::new();
+    for (i, source) in sources.enumerate() {
+        s = match i {
+            0 => format!("{source}"),
+            _ => format!("{source} ({s})"),
+        }
+    }
+    s
+}
