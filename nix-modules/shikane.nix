28b7ce43ed1fba723d05f8d7892295c083ee7374
Hendrik Wolff <hendrik.wolff@agdsn.me>
2023-06-11 02:09:43 +0200

    nix: Split off shikane build definition from flake.nix

---
+++ nix-modules/shikane.nix
@@ -0,0 +1,44 @@
+{ craneLib, pkgs, ... }:
+
+let
+  commonArgs = {
+    src = craneLib.cleanCargoSource (craneLib.path ./..);
+    cargoVendorDir = craneLib.vendorCargoDeps { cargoLock = ./../Cargo.lock; };
+    doCheck = false;
+  };
+
+  cargoArtifacts = craneLib.buildDepsOnly (commonArgs // { });
+
+  shikane-clippy = craneLib.cargoClippy (commonArgs // {
+    inherit cargoArtifacts;
+  });
+
+  shikane = craneLib.buildPackage (commonArgs // {
+    inherit cargoArtifacts;
+  });
+
+  shikane-docs = pkgs.stdenv.mkDerivation {
+    name = "shikane-docs";
+    src = ./..;
+    nativeBuildInputs = with pkgs; [ pandoc installShellFiles ];
+    buildPhase = ''
+      runHook preBuild
+      bash scripts/build-docs.sh man ${shikane.version}
+      runHook postBuild
+    '';
+    installPhase = ''
+      runHook preInstall
+      installManPage build/man/*
+      runHook postInstall
+    '';
+  };
+in
+{
+  default = pkgs.symlinkJoin {
+    name = "shikane";
+    paths = [ shikane shikane-docs ];
+  };
+  shikane = shikane;
+  shikane-docs = shikane-docs;
+  shikane-clippy = shikane-clippy;
+}
