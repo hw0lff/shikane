b375aaa13798fe7bc91d102ac57e388ed78658b8
Hendrik Wolff <hendrik.wolff@agdsn.me>
2023-06-12 17:42:04 +0200

    nix: Add systemd service to silence zsh greeting

--- nix-modules/zsh.nix
+++ nix-modules/zsh.nix
@@ -1,8 +1,25 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  touchy-testy-zshrcy = pkgs.writeShellScript "touchy-testy-zshrcy"
+    ''
+      touch ${config.users.users.testy.home}/.zshrc
+      chown testy:testy ${config.users.users.testy.home}/.zshrc
+    '';
+in
 {
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
   environment.etc.zshrc.text = ''
     source ${pkgs.agdsn-zsh-config}/etc/zsh/zshrc
   '';
+
+  systemd.services.testy-zshrc = {
+    enable = true;
+    description = "touch ${config.users.users.testy.home}/.zshrc to stop the zsh greeting at first login";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ConditionPathExists = "!${config.users.users.testy.home}/.zshrc";
+      ExecStart = "${touchy-testy-zshrcy}";
+    };
+  };
 }
