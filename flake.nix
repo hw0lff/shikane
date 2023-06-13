fbaaf73304563ef333181796e7d2674641a11342
Hendrik Wolff <hendrik.wolff@agdsn.me>
2023-06-13 15:55:10 +0200

    nix: Add testosteron wrapper script

--- flake.nix
+++ flake.nix
@@ -31,7 +31,36 @@
           ]);
           craneLib = crane.lib.${system}.overrideToolchain fenixToolchain;
           shikane-pkg = pkgs.callPackage ./nix-modules/shikane.nix { inherit craneLib pkgs; };
-          testosteron-vm = self.nixosConfigurations.testosteron.config.system.build.vm;
+          testosteron = self.nixosConfigurations.testosteron;
+          testosteron-vm = testosteron.config.system.build.vm;
+          testosteron-vm-wrapper = pkgs.writeScript "testosteron-vm-wrapper"
+            ''
+              # kill old VM
+              kill $(cat /tmp/testosteron.pid 2>/dev/null) 2>/dev/null
+
+              set -euo pipefail
+              echo testosteron: The testing vm for funky graphics
+              echo Now with 100% more graphics!
+              echo
+              echo 'SSH:       localhost:2222'
+              echo 'test-log:  localhost:2233'
+              echo
+              echo root password is \"\" '(empty)'
+              echo testy password is \"\" '(empty)'
+              echo
+              echo QEMU will create a pidfile at /tmp/testosteron.pid
+              echo In case of problems, kill it with:
+              echo
+              echo kill '$(cat /tmp/testosteron.pid)'
+              echo
+
+              echo 'Starting the VM...'
+              ${testosteron-vm}/bin/run-testosteron-vm
+
+              echo 'Watching logs...'
+              echo '========================='
+              ${pkgs.nmap}/bin/ncat localhost 2233 --listen
+            '';
         in
         rec
         {
@@ -41,12 +70,13 @@
             shikane-docs = shikane-pkg.shikane-docs;
             shikane-nextest-archive = shikane-pkg.shikane-nextest-archive;
             testosteron-vm = testosteron-vm;
+            testosteron-vm-wrapper = testosteron-vm-wrapper;
           };
 
           apps.default = apps.test;
           apps.test = {
             type = "app";
-            program = "${self.packages.${system}.testosteron-vm}/bin/run-testosteron-vm";
+            program = "${self.packages.${system}.testosteron-vm-wrapper}";
           };
 
           devShells.default = pkgs.mkShell {
