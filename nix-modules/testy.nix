b9ea45d0edb51385a3fb8cfb4a9bbbe630e6577c
Hendrik Wolff <hendrik.wolff@agdsn.me>
2023-06-14 13:29:46 +0200

    nix: Blacklist the bochs kernel module to have one, unambigous GPU

--- nix-modules/testy.nix
+++ nix-modules/testy.nix
@@ -39,6 +39,8 @@ in
     };
   };
 
+  boot.blacklistedKernelModules = [ "bochs" ];
+
   users.users.testy.packages = [ pkgs.cargo-nextest ];
   systemd.user.services.cargo-nextest = {
     enable = true;
